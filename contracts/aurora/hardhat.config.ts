cff866c055d71290f7814e39fdfd6fcb9a96bae8
Tobias Leinss <caruso33@web.de>
2022-09-28 13:21:12 +0200

    Fix test and deploy

--- contracts/aurora/hardhat.config.ts
+++ contracts/aurora/hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
+import { HardhatUserConfig } from "hardhat/config"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-};
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    aurora_test: {
+      chainId: 1313161555,
+      url: "https://testnet.aurora.dev",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    aurora: {
+      chainId: 1313161554,
+      url: "https://mainnet.aurora.dev",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+  },
+}
 
-export default config;
+export default config
