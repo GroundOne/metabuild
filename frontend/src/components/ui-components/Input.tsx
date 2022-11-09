7010e75ddc31ad1a55e82490039f355316b8a67a
Frangiskos <frangiskos@gmail.com>
2022-11-09 21:57:38 +0200

    feat: add label-right to input

--- frontend/src/components/ui-components/Input.tsx
+++ frontend/src/components/ui-components/Input.tsx
@@ -30,59 +30,38 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             ...otherProps
         },
         ref
-    ) => {
-        return (
-            //     <div className="flex flex-col">
-            //         <label htmlFor={id} className="text-sm font-semibold">
-            //             {placeholder}
-            //             {required && <span className="text-red-500">*</span>}
-            //         </label>
-            //         <input
-            //             ref={ref}
-            //             id={id}
-            //             name={name}
-            //             type={type}
-            //             disabled={isDisabled}
-            //             placeholder={placeholder}
-            //             className={clsx(
-            //                 'border border-gray-300 rounded-md px-4 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent',
-            //                 {
-            //                     'border-red-500': isInvalid,
-            //                 },
-            //                 className
-            //             )}
-            //             {...otherProps}
-            //         />
-            //         {isInvalid && <p className="text-red-500 text-sm">{errorText}</p>}
-            //     </div>
-            // );
-            <div className="flex flex-col">
-                <input
-                    id={id}
-                    name={name}
-                    type={type}
-                    className={clsx(
-                        'mt-10 block w-full rounded-md border-transparent bg-gray-100',
-                        'focus:border-gray-300 focus:bg-white focus:ring-0',
-                        isInvalid ? 'border-red-600 focus:ring-red-600' : '',
-                        isDisabled ? 'bg-gray-200' : '',
-                        labelRight ? `pr-10 after:content-[${labelRight}] absolute top-0` : '',
-                        className
-                    )}
-                    disabled={isDisabled}
-                    //   placeholder={placeholder}
-                    ref={ref}
-                    {...otherProps}
-                ></input>
-                <label htmlFor={id} className="relative block">
-                    {isInvalid && errorText && (
-                        <span className="-mb-3 block py-1 pl-4 text-left text-sm text-red-600">{errorText}</span>
-                    )}
-                    <span className="mt-2 inline-block pl-4 text-gray-600">{placeholder}</span>
-                </label>
-            </div>
-        );
-    }
+    ) => (
+        <div className="relative flex flex-col">
+            <input
+                id={id}
+                name={name}
+                type={type}
+                className={clsx(
+                    'mt-10 block w-full rounded-md border-transparent bg-gray-100',
+                    'focus:border-gray-300 focus:bg-white focus:ring-0',
+                    isInvalid ? 'border-red-600 focus:ring-red-600' : '',
+                    isDisabled ? 'bg-gray-200' : '',
+                    labelRight ? `pr-[56px]` : '',
+                    className
+                )}
+                disabled={isDisabled}
+                //   placeholder={placeholder}
+                ref={ref}
+                {...otherProps}
+            ></input>
+            {labelRight && (
+                <div className="absolute right-2 mt-10 border-l border-gray-300 py-2 pl-[6px] text-black">
+                    {labelRight}
+                </div>
+            )}
+            <label htmlFor={id} className="relative block">
+                {isInvalid && errorText && (
+                    <span className="-mb-3 block py-1 pl-4 text-left text-sm text-red-600">{errorText}</span>
+                )}
+                <span className="mt-2 inline-block pl-4 text-gray-600">{placeholder}</span>
+            </label>
+        </div>
+    )
 );
 Input.displayName = 'Input';
 
