16e6e10f8c5b98ff919b24d37365264a75415360
Frangiskos <frangiskos@gmail.com>
2022-11-09 14:26:17 +0200

    feat: input field labelRight

--- frontend/src/components/ui-components/Input.tsx
+++ frontend/src/components/ui-components/Input.tsx
@@ -11,6 +11,7 @@ interface InputProps extends React.HTMLProps<HTMLInputElement> {
     placeholder: string;
     type?: string;
     isDisabled?: boolean;
+    labelRight?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -24,13 +25,38 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             placeholder,
             required = false,
             isDisabled = false,
+            labelRight = false,
             type = 'text',
             ...otherProps
         },
         ref
     ) => {
         return (
-            <label className="block">
+            //     <div className="flex flex-col">
+            //         <label htmlFor={id} className="text-sm font-semibold">
+            //             {placeholder}
+            //             {required && <span className="text-red-500">*</span>}
+            //         </label>
+            //         <input
+            //             ref={ref}
+            //             id={id}
+            //             name={name}
+            //             type={type}
+            //             disabled={isDisabled}
+            //             placeholder={placeholder}
+            //             className={clsx(
+            //                 'border border-gray-300 rounded-md px-4 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent',
+            //                 {
+            //                     'border-red-500': isInvalid,
+            //                 },
+            //                 className
+            //             )}
+            //             {...otherProps}
+            //         />
+            //         {isInvalid && <p className="text-red-500 text-sm">{errorText}</p>}
+            //     </div>
+            // );
+            <div className="flex flex-col">
                 <input
                     id={id}
                     name={name}
@@ -40,6 +66,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                         'focus:border-gray-300 focus:bg-white focus:ring-0',
                         isInvalid ? 'border-red-600 focus:ring-red-600' : '',
                         isDisabled ? 'bg-gray-200' : '',
+                        labelRight ? `pr-10 after:content-[${labelRight}] absolute top-0` : '',
                         className
                     )}
                     disabled={isDisabled}
@@ -47,11 +74,13 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                     ref={ref}
                     {...otherProps}
                 ></input>
-                {isInvalid && errorText && (
-                    <span className="-mb-3 block py-1 pl-4 text-left text-sm text-red-600">{errorText}</span>
-                )}
-                <span className="mt-2 inline-block pl-4 text-gray-600">{placeholder}</span>
-            </label>
+                <label htmlFor={id} className="relative block">
+                    {isInvalid && errorText && (
+                        <span className="-mb-3 block py-1 pl-4 text-left text-sm text-red-600">{errorText}</span>
+                    )}
+                    <span className="mt-2 inline-block pl-4 text-gray-600">{placeholder}</span>
+                </label>
+            </div>
         );
     }
 );
