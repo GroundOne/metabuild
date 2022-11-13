6581e0d6c61451770a928064af66d00e5ba05a1a
Frangiskos <frangiskos@gmail.com>
2022-11-13 10:17:26 +0200

    feat: add background image

--- frontend/src/components/part-holder/BuyPartConfirm.tsx
+++ frontend/src/components/part-holder/BuyPartConfirm.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useState } from 'react';
 import constants from '../../constants';
 import { convertPropertiesStringToIds } from '../../utils/common';
 import { ContractVarsParsed } from '../../utils/near-interface';
+import AppCard from '../ui-components/AppCard';
 import Button from '../ui-components/Button';
 import { NearContext, WalletState } from '../walletContext';
 
@@ -28,50 +30,61 @@ export default function BuyPartConfirm() {
 
     return (
         <>
-            <div className="font-semibold">PART Sale Statistics</div>
-            <div className="mt-4">
-                <div className="flex flex-col">
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Project Name</div>
-                        <div className="w-2/3 text-black">{contractVars?.projectName}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Project Address</div>
-                        <div className="w-2/3 text-black">{contractVars?.projectAddress}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Total PARTs for sale</div>
-                        <div className="w-2/3 text-black">{contractVars?.totalSupply}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">PARTs Reserved</div>
-                        <div className="w-2/3 text-black">
-                            {contractVars?.reservedTokenIds?.length
-                                ? contractVars.reservedTokenIds.length + ' (' + contractVars?.reservedTokens + ')'
-                                : 'None'}
+            <Image
+                id="project-image"
+                className="fixed top-0 left-0 z-[-100] h-full w-full object-cover"
+                alt=""
+                object-fit="cover"
+                width={1920}
+                height={1080}
+                src="https://images.squarespace-cdn.com/content/v1/63283ec16922c81dc0f97e2f/e3150b7f-bfc8-4251-ad50-3344f4b21b3d/image.jpg"
+            />
+            <AppCard>
+                <div className="font-semibold">PART Sale Statistics</div>
+                <div className="mt-4">
+                    <div className="flex flex-col">
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Project Name</div>
+                            <div className="w-2/3 text-black">{contractVars?.projectName}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Project Address</div>
+                            <div className="w-2/3 text-black">{contractVars?.projectAddress}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Total PARTs for sale</div>
+                            <div className="w-2/3 text-black">{contractVars?.totalSupply}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">PARTs Reserved</div>
+                            <div className="w-2/3 text-black">
+                                {contractVars?.reservedTokenIds?.length
+                                    ? contractVars.reservedTokenIds.length + ' (' + contractVars?.reservedTokens + ')'
+                                    : 'None'}
+                            </div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Sale Opening</div>
+                            <div className="w-2/3 text-black">{contractVars?.saleOpeningDate.toLocaleDateString()}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Sale Close</div>
+                            <div className="w-2/3 text-black">{contractVars?.saleCloseDate.toLocaleDateString()}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Parts Sold</div>
+                            <div className="w-2/3 text-black">{contractVars?.currentTokenId ?? 1 - 1}</div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-1/3">Price of PART</div>
+                            <div className="w-2/3 text-black">{contractVars?.priceLabel} NEAR</div>
                         </div>
                     </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Sale Opening</div>
-                        <div className="w-2/3 text-black">{contractVars?.saleOpeningDate.toLocaleDateString()}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Sale Close</div>
-                        <div className="w-2/3 text-black">{contractVars?.saleCloseDate.toLocaleDateString()}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Parts Sold</div>
-                        <div className="w-2/3 text-black">{contractVars?.currentTokenId ?? 1 - 1}</div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-1/3">Price of PART</div>
-                        <div className="w-2/3 text-black">{contractVars?.priceLabel} NEAR</div>
-                    </div>
                 </div>
-            </div>
-            <Button isInvertedColor size="md" className="mt-10" onClick={handlePurchase}>
-                CONFIRM PART PURCHASE FOR {contractVars?.priceLabel} NEAR
-            </Button>
+                <Button isInvertedColor size="md" className="mt-10" onClick={handlePurchase}>
+                    CONFIRM PART PURCHASE FOR {contractVars?.priceLabel} NEAR
+                </Button>
+            </AppCard>
         </>
     );
 }
