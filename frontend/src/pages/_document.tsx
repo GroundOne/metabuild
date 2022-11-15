edef61d1a6ce378ebfe76c65a74c3f137d76c382
Frangiskos <frangiskos@gmail.com>
2022-11-15 19:57:58 +0200

    feat: add favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <meta charSet="utf-8" />
-                {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" /> */}
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
             </Head>
             <body>
                 <Main />
