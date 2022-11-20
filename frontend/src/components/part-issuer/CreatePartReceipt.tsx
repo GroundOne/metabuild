3413ba4ea137bcdc2953c4393dfdc031b4a1025b
Frangiskos <frangiskos@gmail.com>
2022-11-20 23:09:05 +0200

    refactor: extract email functionality to a separate component

--- frontend/src/components/part-issuer/CreatePartReceipt.tsx
+++ frontend/src/components/part-issuer/CreatePartReceipt.tsx
@@ -1,65 +1,13 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import Link from 'next/link';
-import { useState } from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { env } from '../../constants';
 import { ContractVarsParsed } from '../../utils/near-interface';
 import Button from '../ui-components/Button';
-import Input from '../ui-components/Input';
-
-const emailSchema = yup.object({
-    email: yup.string().label('Email address').email().required(),
-});
-type EmailFormValue = yup.InferType<typeof emailSchema>;
+import SendCopyToEmail from '../ui-components/SendCopyToEmail';
 
 const CreatePartReceipt: React.FC<{ contractVars: ContractVarsParsed; transactionHashes: string }> = ({
     contractVars,
     transactionHashes,
 }) => {
-    const [emailSent, setEmailSent] = useState<{
-        status: 'not_send' | 'send' | 'sending';
-        error: boolean;
-        message: string;
-    }>({ status: 'not_send', error: false, message: '' });
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<EmailFormValue>({ resolver: yupResolver(emailSchema) });
     const userLocale = navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
-
-    const onSubmit = async (data: EmailFormValue) => {
-        try {
-            setEmailSent({ status: 'sending', error: false, message: '' });
-            const api = env.NEXT_PUBLIC_API_URL + '/send-email';
-            const response = await fetch(api, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    to: data.email,
-                    template: 'transactionalEmail',
-                    subject: 'GroundOne: Your part has been issued',
-                    message: renderToStaticMarkup(receiptInfo),
-                }),
-            });
-
-            const { success, message } = await response.json();
-            if (response.status !== 200) {
-                setEmailSent({ status: 'not_send', error: true, message: 'Email was not sent! Try again.' });
-            } else if (success) {
-                setEmailSent({ status: 'send', error: false, message });
-            } else {
-                setEmailSent({ status: 'not_send', error: true, message });
-            }
-        } catch (error: any) {
-            setEmailSent({ status: 'not_send', error: true, message: error.message });
-        }
-    };
 
     const receiptInfo = (
         <div>
@@ -126,51 +74,6 @@ const CreatePartReceipt: React.FC<{ contractVars: ContractVarsParsed; transactio
         </div>
     );
 
-    return (
-        <>
-            {receiptInfo}
-
-            {emailSent.status !== 'send' && (
-                <form className="mt-4" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="grid grid-cols-2 gap-x-5">
-                        <Input
-                            id="email"
-                            type="text"
-                            placeholder="Receive this receipt by email."
-                            isDisabled={emailSent.status === 'sending'}
-                            isInvalid={!!errors.email}
-                            errorText={errors.email?.message as string | undefined}
-                            {...register('email')}
-                        />
-                        <div>
-                            <Button
-                                isLoading={emailSent.status === 'sending'}
-                                type="submit"
-                                isInvertedColor
-                                size="sm"
-                                className="mt-10"
-                            >
-                                {emailSent.status === 'sending' ? `Sending…` : `Send Email`}
-                            </Button>
-                        </div>
-                    </div>
-                </form>
-            )}
-            {emailSent.status === 'send' && (
-                <div className="mt-10">
-                    <p className="text-center">
-                        <b>The receipt has been sent to your email address</b>
-                    </p>
-                </div>
-            )}
-            {emailSent.error && (
-                <div className="mt-10">
-                    <p className="text-center">
-                        <b>{emailSent.message}</b>
-                    </p>
-                </div>
-            )}
-        </>
-    );
+    return <SendCopyToEmail subject="GroundOne: Your part has been issued">{receiptInfo}</SendCopyToEmail>;
 };
 export default CreatePartReceipt;
