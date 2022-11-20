83f8afc870ccc761e52cec08dfae0ef1d5120b4c
Frangiskos <frangiskos@gmail.com>
2022-11-20 20:52:59 +0200

    feat: redirect to main page when on app.groundone.io

--- frontend/src/components/ui-components/Logo.tsx
+++ frontend/src/components/ui-components/Logo.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Logo() {
+    const homeLink = window?.location?.host === 'app.groundone.io' ? 'https://www.groundone.io' : '/';
+
     return (
         <div className="flex min-h-full justify-start lg:w-0 lg:flex-1">
-            <Link href="/" passHref>
+            <Link href={homeLink} passHref>
                 <Image
                     priority
                     className="h-16 w-auto cursor-pointer"
